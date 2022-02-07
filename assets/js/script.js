200ccca871bb189074358ec286f931583ac44ec0
Fernando Gortari <fernando.gortari@gmail.com>
2022-02-07 13:44:35 -0400

    desafío finalizado

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,12 @@
-//IIFE
-const mostrarVideo = (() => {
-    let mostrar = (url, idIframe) => document.getElementById(idIframe).setAttribute("src", url);
+//1. Implementar el Patrón Módulo mediante IIFE
+const showVideo = (() => {
+    let show = (url, idIframe) => document.getElementById(idIframe).setAttribute("src", url);
     return{
-        verVideo: (url, idIframe) => mostrar(url, idIframe)
+        watchVideo: (url, idIframe) => show(url, idIframe)
     }    
 })();
 
-
+//2. Establecer una clase padre denominada Multimedia
 class Multimedia{
     constructor(url){
         let _url = url;
@@ -21,6 +21,7 @@ class Multimedia{
     }
 }
 
+//3. Crear una clase “Reproductor”, siendo hija de la clase padre Multimedia
 class Reproductor extends Multimedia{
     constructor(url, id){
     super(url);
@@ -32,18 +33,26 @@ class Reproductor extends Multimedia{
         return this.getId();
     }
     playMultimedia = () => {
-        mostrarVideo.verVideo(this.obtainingUrl, this.obtainingId);
+        showVideo.watchVideo(this.obtainingUrl, this.obtainingId);
     }
-    setInicio = (start) => {
-        document.getElementById(this.obtainingId).setAttribute('src', this.obtainingUrl+start)
+    setInicio = (time) => {
+        document.getElementById(this.obtainingId).setAttribute('src', this.obtainingUrl+`?start=${time}`);
     }
 }
 
-let musica = new Reproductor('https://www.youtube.com/embed/vlhaFtsnqfs', 'musica');
-let peliculas = new Reproductor('https://www.youtube.com/embed/fWQrd6cwJ0A', 'peliculas');
+// 4. Instanciar la clase hija pasando como argumento la URL y el id para cada etiqueta
+// iframe, por lo que se deben crear tres instancias, una para música, otra para película
+// y otra para serie, con sus respectivas URL.
+let music = new Reproductor('https://www.youtube.com/embed/vlhaFtsnqfs', 'musica');
+let movies = new Reproductor('https://www.youtube.com/embed/fWQrd6cwJ0A', 'peliculas');
 let series = new Reproductor('https://www.youtube.com/embed/kAYc5elCJcE', 'series');
 
-musica.playMultimedia();
-musica.setInicio('?start=82');
-peliculas.playMultimedia();
+// 5. Invocar al método “playMultimedia” para cada instancia creada, mostrando así los
+// videos en el documento HTML.
+music.playMultimedia();
+movies.playMultimedia();
 series.playMultimedia();
+
+// 6. Utiliza el método “setInicio” para modificar el tiempo de inicio en alguna de las
+// instancias creadas.
+music.setInicio('82');
